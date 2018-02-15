328118b20506721b6bb68ea925a571278c256002
Ian Vlahovic <banddorkfht7@gmail.com>
2018-02-15 03:25:01 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
-def alphabetize(arr)
-  # code here
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(glossary)
+  glossary.sort_by do |phrase|
+    phrase.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
