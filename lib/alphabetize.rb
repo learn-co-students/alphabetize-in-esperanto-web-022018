3ed954bced66dad51908d312ec99a702abcccf2d
alec-petros <a.petros@verizon.net>
2018-02-16 15:43:38 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  output = arr.sort_by {|string| string.chars.map{|c| esperanto_alphabet.index(c)}}
+  output
 end
